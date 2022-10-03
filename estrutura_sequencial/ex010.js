2eadd28660558f7cdc7eddeb290079a6587db886
RFAlves2022 <94083934+RFAlves2022@users.noreply.github.com>
2022-10-03 15:54:29 -0300

    .

--- estrutura_sequencial/ex010.js
+++ estrutura_sequencial/ex010.js
@@ -1,7 +1,11 @@
 //Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 
-var downloadMB, speedInternet, minDownload
-
+function download() {
+    let tamanho = document.querySelector("#tamanhoArquivo").value
+    let velocidade = document.querySelector("#velicidadeInternet").value
+    let tempoDownload = (tamanho / (velocidade / 8)) / 60
+    document.querySelector("#resultado").innerHTML = `<strong> O arquivo vai demorar ${tempoDownload.toFixed(2)} minutos</strong>`
+}
 
 
 
